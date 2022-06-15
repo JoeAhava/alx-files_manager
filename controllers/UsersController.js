046899afcfe75fd6145a64fad516d146db38af1e
Yohannes Terfa <lightshadow011@gmail.com>
2022-06-15 20:12:05 +0300

    user and auth controller

---
+++ controllers/UsersController.js
@@ -0,0 +1,40 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+export default class UsersController {
+  // eslint-disable-next-line consistent-return
+  static async postNew(req, res) {
+    const email = req.body.email ? req.body.email : null;
+    let password = req.body.password ? req.body.password : null;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+    const exist = await dbClient.db.collection('users').findOne({ email });
+
+    if (exist) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+    password = sha1(password);
+    const addToDb = await dbClient.db.collection('users').insertOne({ email, password });
+    const id = addToDb.insertedId.toString();
+
+    res.status(201).json({ email, id });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['X-Token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    // Basic Authentication
+    const basic = req.headers.get('Authorization').split(' ')[1];
+    const credentials = Buffer.from(basic, 'base64').toString('ascii');
+    const [_email] = credentials.split(':');
+
+    return res.status(200).json({ email: _email, id: res._id.toString });
+  }
+}
